c465fdec685e98191cd9f08efbeb5d2b4eeeb0b2
Karl John Villardar <74708425+kaimatzu@users.noreply.github.com>
2023-11-10 22:45:05 +0800

    Rustify (rinf)

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'package:rinf/rinf.dart';
 
 void main() async {
+  await Rinf.ensureInitialized();
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
