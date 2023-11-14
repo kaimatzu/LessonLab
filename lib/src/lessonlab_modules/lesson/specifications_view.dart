50d6e825789e7d8059cd67cf73f793fb3d050c6b
Yuurei <markyuri.geagonia@cit.edu>
2023-11-15 00:09:20 +0800

    Add cancel and confirm buttons

--- lib/src/lessonlab_modules/lesson/specifications_view.dart
+++ lib/src/lessonlab_modules/lesson/specifications_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lessonlab/src/global_components/lessonlab_appbar.dart';
+import 'package:lessonlab/src/global_components/primary_button.dart';
+import 'package:lessonlab/src/global_components/secondary_button.dart';
 import 'package:lessonlab/src/lessonlab_modules/lesson/components/dropdown_menu.dart';
 import 'package:lessonlab/src/lessonlab_modules/lesson/components/input_field.dart';
+import 'package:lessonlab/src/lessonlab_modules/material_selection/material_selection_view.dart';
 
 class SpecificationsView extends StatelessWidget {
   const SpecificationsView({
@@ -13,16 +16,39 @@ class SpecificationsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(
-      appBar: LessonLabAppBar(),
+    return Scaffold(
+      appBar: const LessonLabAppBar(),
       body:Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.fromLTRB(180.0, 0.0, 180.0, 60.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              InputFields(label: 'Title', hintLabel: 'Enter lesson title'),
-              InputFields(label: 'Timeframe', hintLabel: 'Enter timeframe'),
-              Dropdown()
+              const InputFields(label: 'Title', hintLabel: 'Enter lesson title'),
+              const InputFields(label: 'Timeframe', hintLabel: 'Enter timeframe'),
+              const InputFields(label: 'Learning Outcomes', hintLabel: 'Enter learning outcomes'),
+              const Dropdown(),
+              Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 30.0, 16.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SecondaryButton(
+                      handlePress: () {
+                        Navigator.restorablePushNamed(
+                          context, 
+                          MaterialSelectionView.routeName
+                          );
+                      },
+                      text: 'Cancel',
+                    ),
+                    const SizedBox(width: 30.0),
+                    PrimaryButton(
+                      handlePress: () {},
+                      text: 'Confirm',
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         )
