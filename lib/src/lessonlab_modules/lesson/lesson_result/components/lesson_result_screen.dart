dda95f5b847af93b505b92a57807dee8dad55371
Karl John Villardar <74708425+kaimatzu@users.noreply.github.com>
2023-11-29 21:23:06 +0800

    Created streaming connection from Python -> Rust -> Flutter. Still need to fix recieving data.

--- lib/src/lessonlab_modules/lesson/lesson_result/components/lesson_result_screen.dart
+++ lib/src/lessonlab_modules/lesson/lesson_result/components/lesson_result_screen.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:lessonlab/src/lessonlab_modules/lesson/lesson_result/components/text_editor.dart';
 import 'package:lessonlab/src/lessonlab_modules/lesson/lesson_result/lesson_result_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:rinf/rinf.dart';
+import 'package:lessonlab/messages/results/view_lesson_result/load_lesson.pb.dart'
+    as streamMessage;
 
 class LessonResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Make the same as Menu.
     final lessonResultViewModel = context.watch<LessonResultViewModel>();
-
+    var message = "";
+    
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,6 +59,27 @@ class LessonResultScreen extends StatelessWidget {
               },
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(120.0, 0.0, 120.0, 30.0),
+            child: StreamBuilder<RustSignal>(
+              stream: rustBroadcaster.stream.where((rustSignal) {
+                return rustSignal.resource == streamMessage.ID;
+              }),
+              builder: (context, snapshot) {
+                final rustSignal = snapshot.data;
+                if (rustSignal == null) {
+                  return Text("Nothing received yet");
+                } else {
+                  final signal = streamMessage.StateSignal.fromBuffer(
+                    rustSignal.message!,
+                  );
+                  final rinfMessage = signal.streamMessage;
+                  message += rinfMessage;
+                  return Text(message);
+                }
+              },
+            ),
+          )
         ],
       ),
     );
--- native/hub/src/app/utils/lesson_generator.rs
+++ native/hub/src/app/utils/lesson_generator.rs
@@ -27,4 +27,31 @@ pub fn generate(lesson_source: String) -> PyResult<String> {
     })
 }
 
+pub fn generate_lesson_stream() -> PyResult<String> {
+    let lesson_source = match scrapers::scrape_pdf(String::from("C:/Users/karlj/OneDrive/Documents/Proxy Design Pattern Summary.pdf")) {
+        Ok(source) => {
+            print!("{}", source);
+            println!("\nGenerated Lesson:_______________________________________________________________\n");
+            source
+        }
+        Err(_) => return Err(PyErr::new::<exceptions::PyException, _>("Failed to scrape PDF")),
+    };
 
+    pyo3::prepare_freethreaded_python();
+    Python::with_gil(|py| {
+
+        let lesson_generator = PyModule::from_code(
+            py,
+            include_str!("python/lesson_generator.py"),
+            "lesson_generator.py",
+            "lesson_generator",
+        )?;
+
+        let generated_lesson: String = lesson_generator
+        .getattr("generate_lesson_stream")?
+        .call((lesson_source,), None)?
+        .extract()?;
+
+        Ok(generated_lesson)
+    })
+}
