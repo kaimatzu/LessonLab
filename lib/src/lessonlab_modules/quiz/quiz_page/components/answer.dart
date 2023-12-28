94b80f72674e1aefe55062f4a86bfe77aed195ac
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-12-28 17:03:26 +0800

    Implement answering and next question

--- lib/src/lessonlab_modules/quiz/quiz_page/components/answer.dart
+++ lib/src/lessonlab_modules/quiz/quiz_page/components/answer.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final String answerText;
+  final Color answerColor;
+  final Function() answerTap;
 
-  Answer({required this.answerText});
+  Answer(
+      {required this.answerText,
+      required this.answerColor,
+      required this.answerTap});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: answerTap,
       child: Container(
         padding: const EdgeInsets.all(15.0),
         margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
         width: double.infinity,
         decoration: BoxDecoration(
-            color: null,
+            color: answerColor,
             border: Border.all(color: Colors.blue),
             borderRadius: BorderRadius.circular(20.0)),
         child: Text(
