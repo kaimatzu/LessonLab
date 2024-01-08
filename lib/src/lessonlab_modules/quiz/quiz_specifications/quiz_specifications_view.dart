86cc0d872b7020d91b14751e5662a4f685cdc186
EdCamarillo <edwardjohn.camarillo@cit.edu>
2024-01-08 17:44:32 +0800

    UI for quiz result

--- lib/src/lessonlab_modules/quiz/quiz_specifications/quiz_specifications_view.dart
+++ lib/src/lessonlab_modules/quiz/quiz_specifications/quiz_specifications_view.dart
@@ -22,70 +22,68 @@ class QuizSpecificationsView extends StatelessWidget {
       appBar: const LessonLabAppBar(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20.0, 20.0, 30.0, 0.0),
-        child: Row(
-          children: [ 
-            // Left side
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
-                  ),
-                  color: Color.fromARGB(255, 244, 245, 247),
+        child: Row(children: [
+          // Left side
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 ),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(40.0, 32.0, 30.0, 32.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: quizSpecificationsViewModel.formFields
-                          .map<Widget?>((formField) {
-                            if (formField.inputField != null) {
-                              return formField.inputField;
-                            } else if (formField.textArea != null) {
-                              return formField.textArea;
-                            } else if (formField.dropdown != null) {
-                              return formField.dropdown;
-                            } else if (formField.numberField != null) {
-                              return formField.numberField;
-                            } else {
-                              return null;
-                            }
-                          })
-                          .whereType<Widget>() // Filter out null values
-                          .toList(),
-                    ),
+                color: Color.fromARGB(255, 244, 245, 247),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(40.0, 32.0, 30.0, 32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: quizSpecificationsViewModel.formFields
+                        .map<Widget?>((formField) {
+                          if (formField.inputField != null) {
+                            return formField.inputField;
+                          } else if (formField.textArea != null) {
+                            return formField.textArea;
+                          } else if (formField.dropdown != null) {
+                            return formField.dropdown;
+                          } else if (formField.numberField != null) {
+                            return formField.numberField;
+                          } else {
+                            return null;
+                          }
+                        })
+                        .whereType<Widget>() // Filter out null values
+                        .toList(),
                   ),
                 ),
               ),
             ),
-            // Right side (Ha o mukidashite pa-ppa-ppa)
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SecondaryButton(
+          ),
+          // Right side (Ha o mukidashite pa-ppa-ppa)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SecondaryButton(
                         handlePress: () {
                           quizSpecificationsViewModel.cancelQuiz(context);
                         },
                         text: 'Cancel',
-                        width: 120.0
-                      ),
-                      // const SizedBox(width: 30.0),
-                      // PrimaryButton(
-                      //   handlePress: () {
-                      //     lessonSpecificationsViewModel.addCustomSpecifications();
-                      //   },
-                      //   text: 'Add',
-                      //   enabled: true,
-                      // ),
-                      const SizedBox(width: 8.0),
-                      PrimaryButton(
+                        width: 120.0),
+                    // const SizedBox(width: 30.0),
+                    // PrimaryButton(
+                    //   handlePress: () {
+                    //     lessonSpecificationsViewModel.addCustomSpecifications();
+                    //   },
+                    //   text: 'Add',
+                    //   enabled: true,
+                    // ),
+                    const SizedBox(width: 8.0),
+                    PrimaryButton(
                         handlePress: () {
                           //quizSpecificationsViewModel.collectFormTextValues();
                           quizSpecificationsViewModel.generateQuiz(context);
@@ -94,15 +92,13 @@ class QuizSpecificationsView extends StatelessWidget {
                         },
                         text: 'Start',
                         enabled: true,
-                        width: 120.0
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                        width: 120.0),
+                  ],
+                ),
+              ],
             ),
-          ]
-        ),
+          ),
+        ]),
       ),
     );
   }
