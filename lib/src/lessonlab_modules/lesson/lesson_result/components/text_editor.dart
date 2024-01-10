46b0e84830029c1f081b73d8a35741dcc03237a2
Karl John Villardar <74708425+kaimatzu@users.noreply.github.com>
2024-01-11 01:26:46 +0800

    auto scroll down md renderer

--- lib/src/lessonlab_modules/lesson/lesson_result/components/text_editor.dart
+++ lib/src/lessonlab_modules/lesson/lesson_result/components/text_editor.dart
@@ -54,7 +54,6 @@ class _TextEditor extends State<TextEditor> {
     // final lessonResultViewModel = context.watch<LessonResultViewModel>(); // PROBLEM HERE
     super.initState();
     _scrollController = ScrollController();
-
     var mdDocument = md.Document(
         encodeHtml: false,
         extensionSet: md.ExtensionSet.gitHubFlavored,
@@ -80,14 +79,15 @@ class _TextEditor extends State<TextEditor> {
         markdownContent += rinfMessage;
         if(markdownContent.isNotEmpty) {
           _controller.document = Document.fromDelta(mdToDelta.convert(markdownContent));
+          _controller.moveCursorToEnd();
         }
         // _controller.document.insert(_controller.plainTextEditingValue.text.length - 1, rinfMessage);
       }
       setState(() {
         // message = rinfMessage;
-        _scrollController.jumpTo(
-          _scrollController.position.maxScrollExtent,
-        );
+        // _scrollController.jumpTo(
+        //   _scrollController.position.maxScrollExtent,
+        // );
       });
     });
   }
@@ -97,10 +97,7 @@ class _TextEditor extends State<TextEditor> {
     final lessonResultViewModel = context.watch<LessonResultViewModel>();
     
     // var message = "";
-    lessonResultViewModel.lessonContent = _controller.document.toPlainText();
-    if(_doneGenerating) {
-      lessonResultViewModel.done = true;
-    }
+    
 
     var mdDocument = md.Document(
         encodeHtml: false,
@@ -120,6 +117,13 @@ class _TextEditor extends State<TextEditor> {
       //   EmbeddableTable.tableType: EmbeddableTable.fromMdSyntax,
       // },
     );
+
+    final deltaToMd = DeltaToMarkdown();
+
+    lessonResultViewModel.lessonContent = deltaToMd.convert(_controller.document.toDelta());
+    if(_doneGenerating) {
+      lessonResultViewModel.done = true;
+    }
 
     // const markdown = "# test";
 
